f4d5e9b0d58ca1de0f6932c44f39278453b64753
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-06-11 15:29:55 +0200

    add all needed packages

--- profiles/signals.py
+++ profiles/signals.py
@@ -1,8 +1,8 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from django.contrib.auth.models import User
-from .models import UserProfile, Skill, Experience, Education, Endorsement
+from .models import User, UserProfile, Skill, Experience, Education, Endorsement
 from notifications.models import Notification
+from connections.models import Connection
 
 # Signal to create a user profile when a new user is created
 @receiver(post_save, sender=User)
