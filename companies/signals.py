5bc35d76627d47612f6ee58545ede55dab8a2791
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-06-11 20:51:02 +0200

    Add companies signals

---
+++ companies/signals.py
@@ -0,0 +1,50 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from .models import Company, CompanyUpdate
+from profiles.models import User, UserProfile
+from notifications.models import Notification
+
+# Signal to create a company profile when a new user is created
+@receiver(post_save, sender=UserProfile)
+def create_company_profile(sender, instance, created, **kwargs):
+    if created:
+        Company.objects.create(name=f"{instance.user.username}'s Company")
+
+# Signal to send notification when a new company update is created
+@receiver(post_save, sender=CompanyUpdate)
+def send_company_update_notification(sender, instance, created, **kwargs):
+    if created:
+        company_followers = instance.company.followers.all()
+        notification_message = f"New update from {instance.company.name}: {instance.title}"
+        for follower in company_followers:
+            Notification.objects.create(user=follower, message=notification_message)
+
+# Signal to update follower count when a user follows or unfollows a company
+@receiver(post_save, sender=Company.followers.through)
+@receiver(post_delete, sender=Company.followers.through)
+def update_follower_count(sender, instance, **kwargs):
+    company = instance.company
+    company.follower_count = company.followers.count()
+    company.save()
+
+# Signal to update company member count when a user joins or leaves a company
+@receiver(post_save, sender=Company.members.through)
+@receiver(post_delete, sender=Company.members.through)
+def update_member_count(sender, instance, **kwargs):
+    company = instance.company
+    company.member_count = company.members.count()
+    company.save()
+
+# Signal to delete associated company updates when a company is deleted
+@receiver(post_delete, sender=Company)
+def delete_associated_company_updates(sender, instance, **kwargs):
+    CompanyUpdate.objects.filter(company=instance).delete()
+
+# Signal to notify admins when a new company is created
+@receiver(post_save, sender=Company)
+def notify_admins_on_company_creation(sender, instance, created, **kwargs):
+    if created:
+        admin_profiles = UserProfile.objects.filter(user__is_staff=True)
+        notification_message = f"New company created: {instance.name}"
+        for admin_profile in admin_profiles:
+            Notification.objects.create(user=admin_profile, message=notification_message)
