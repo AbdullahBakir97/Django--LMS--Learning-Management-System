1124b7770f2e9eea531e62c17ca9f38b1b98e66c
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-06-13 07:27:50 +0200

    register posts admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'content_preview', 'group', 'visibility', 'created_at')
+    list_filter = ('visibility', 'created_at', 'author')
+    search_fields = ('content', 'author__username', 'group__name')
+    date_hierarchy = 'created_at'
+    filter_horizontal = ('categories', 'likes', 'reactions', 'comments', 'shares', 'tags')
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        (None, {
+            'fields': ('author', 'group', 'content', 'attachments', 'visibility', 'categories')
+        }),
+        ('Advanced Options', {
+            'fields': ('likes', 'reactions', 'comments', 'shares', 'tags'),
+            'classes': ('collapse',),
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',),
+        }),
+    )
+
+    def content_preview(self, obj):
+        return obj.content[:50] + '...' if len(obj.content) > 50 else obj.content
+
+    content_preview.short_description = 'Content'
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'post', 'content_preview', 'visibility', 'created_at')
+    list_filter = ('visibility', 'created_at', 'author')
+    search_fields = ('content', 'author__username', 'post__content')
+    date_hierarchy = 'created_at'
+    filter_horizontal = ('attachments', 'reactions')
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        (None, {
+            'fields': ('post', 'author', 'content', 'attachments', 'visibility')
+        }),
+        ('Advanced Options', {
+            'fields': ('parent_comment', 'replies', 'reactions'),
+            'classes': ('collapse',),
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',),
+        }),
+    )
+
+    def content_preview(self, obj):
+        return obj.content[:50] + '...' if len(obj.content) > 50 else obj.content
+
+    content_preview.short_description = 'Content'
